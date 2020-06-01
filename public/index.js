bc2c63c6987381cc4af5bb1ff98ecb277302bdbd
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-01 20:10:27 +0300

    refactor: colors moved to the separate file, because there will be a lot

--- public/index.js
+++ public/index.js
@@ -2,18 +2,24 @@ import React from 'react'
 import { render } from 'react-dom'
 import { LuiApp } from '../lui/app'
 import { LuiContent } from '../lui/content'
+import { LuiNotification } from '../lui/notification'
 import { LuiPageTitle } from '../lui/pageTitle'
 import { LuiGrid, LuiGrid_item } from '../lui/grid'
+import { LuiSection } from '../lui/section'
 
 const App = () => (
-		<LuiApp>
-			<LuiContent>
-				<LuiPageTitle>CTDL-UI Components</LuiPageTitle>
-				<LuiGrid airy>
-					<LuiGrid_item skinny>1</LuiGrid_item>
-					<LuiGrid_item skinny>2</LuiGrid_item>
-				</LuiGrid>
-			</LuiContent>
-		</LuiApp>
+	<LuiApp>
+		<LuiContent>
+			<LuiPageTitle>CTDL-UI Components</LuiPageTitle>
+			<LuiSection>
+				<LuiNotification>This is Notification</LuiNotification>
+			</LuiSection>
+			<LuiGrid>
+				<LuiGrid_item>This</LuiGrid_item>
+				<LuiGrid_item>Is</LuiGrid_item>
+				<LuiGrid_item>Grid</LuiGrid_item>
+			</LuiGrid>
+		</LuiContent>
+	</LuiApp>
 )
 render(<App/>, document.getElementById('app'))
