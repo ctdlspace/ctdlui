f5a0d0d09c5644885a515580b6c9c9546956abba
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-01 17:37:47 +0300

    chore: some dev preview

--- public/index.js
+++ public/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { LuiApp } from '../lui/app'
+import { LuiContent } from '../lui/content'
+import { LuiPageTitle } from '../lui/pageTitle'
+import { LuiGrid, LuiGrid_item } from '../lui/grid'
 
 const App = () => (
-	<div>react yo</div>
+		<LuiApp>
+			<LuiContent>
+				<LuiPageTitle>CTDL-UI Components</LuiPageTitle>
+				<LuiGrid airy>
+					<LuiGrid_item skinny>1</LuiGrid_item>
+					<LuiGrid_item skinny>2</LuiGrid_item>
+				</LuiGrid>
+			</LuiContent>
+		</LuiApp>
 )
-
 render(<App/>, document.getElementById('app'))
