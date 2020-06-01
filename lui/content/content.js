8dbe902465d8785fa4d810bbaa64ef00dcc2ce4e
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-01 20:08:41 +0300

    fix: LuiContent Component do not have vertical padding any longer

--- lui/content/content.js
+++ lui/content/content.js
@@ -1,5 +1,8 @@
 /** Content is used for any sort of content that user have to view
- *  Content block should not be used as wrapper, because it's normally have own padding
+ *  Content block should not be used as wrapper, because it's normally have own padding.
+ *  Content works good with Section
+ *
+ *  @see Section
  *  @author rusinozemtsev
  *  @version 0.0.1
  */
