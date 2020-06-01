7cc57227eb5e1f59d2972136b5ebe43cfdae7f74
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-01 17:38:30 +0300

    feat:  LuiPageTitle

---
+++ lui/pageTitle/pageTitle.js
@@ -0,0 +1,20 @@
+/** Description
+ *  @author rusinozemtsev
+ *  @version 0.0.1
+ */
+
+import React from 'react'
+import './pageTitle.less'
+
+/** PageTitle
+ *  @param props
+ *  @param props.children
+ *  @return {*}
+ */
+export function LuiPageTitle(props) {
+	return (
+		<div className="luiPageTitle">
+			{props.children}
+		</div>
+	)
+}
