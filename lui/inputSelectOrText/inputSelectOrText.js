9f8371e36bb05186d122463792eedb188e896618
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-02 12:31:31 +0300

    fix: component sets value incorrect

--- lui/inputSelectOrText/inputSelectOrText.js
+++ lui/inputSelectOrText/inputSelectOrText.js
@@ -1,37 +1,44 @@
+/** Description
+ *  @author rusinozemtsev
+ *  @version 0.0.1
+ */
+
 import React, { useState } from 'react'
-import './inputSelectOrText.less'
 import { LuiInputSelect } from '../inputSelect'
 import { LuiInputText } from '../inputText/inputText'
+import './inputSelectOrText.less'
 
-/** InputSelectOrText "Other option will be added"
- *                    if you want to customize pass {title: "Custom Other"} with "?" as _id
+/** "Other" option will be added
+ *  if you want to customize pass {title: "Custom Other"} with "" as _id
  *
  *  @param props
- *  @param props.name
- *  @param props.value
+ *  @param {string} props.name
+ *  @param {string} props.value
  *  @param {[]} props.options
  *  @return {*}
  */
 export function LuiInputSelectOrText(props) {
 	const { options = [], name } = props
 	const [value, setValue] = useState(props.value)
-	const [isCustomShown, setIsCustomShown] = useState(false)
 	const onChange = e => {
-		if (e.target.value === '?') {
-			setIsCustomShown(true)
-		} else {
-			setIsCustomShown(false)
-		}
 		setValue(e.target.value)
 	}
+	const isCustomShown = !options.find(({_id}) => _id === value) || value === ''
 	return (
 		<div className="luiInputSelectOrText">
 			<div>
-				<LuiInputSelect options={options} value={value} onChange={onChange}/>
+				<LuiInputSelect
+					options={options}
+					value={value}
+					onChange={onChange}/>
 			</div>
-			{isCustomShown && <div>
-				<LuiInputText name={name} value={value}/>
-			</div>}
+			{isCustomShown && (
+				<div>
+					<LuiInputText
+						name={name}
+						value={value}/>
+				</div>
+			)}
 		</div>
 	)
 }
