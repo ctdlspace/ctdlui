881a9677e70a40df6320a1dafc28ef4297eb09e4
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-02 12:58:06 +0300

    refactor: renamed to LuiButton

--- lui/button/button.js
+++ lui/button/button.js
@@ -1,9 +1,17 @@
+import React from 'react'
+import { luiCls } from '../../helpers'
+import './button.less'
+
 /** Button
  *  @param props
  *  @param props.children
  *  @param {function} [props.onClick]
  *  @return {*}
  */
-export function Button (props) {
-	return <button {...props}>{props.children}</button>
+export function LuiButton(props) {
+	return (
+		<button
+			onClick={props.onClick}
+			{...luiCls('luiButton')}>{props.children}</button>
+	)
 }
