34a8fdb03e921c1f4489c0104d7823e228dd0878
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-03 22:55:20 +0300

    docs(LuiPageTitle): improve comments

--- lui/pageTitle/pageTitle.js
+++ lui/pageTitle/pageTitle.js
@@ -1,15 +1,16 @@
-/** Description
- *  @author rusinozemtsev
- *  @version 0.0.1
+/**
+ * Description
+ * @author rusinozemtsev
+ * @version 0.0.1
  */
-
 import React from 'react'
 import './pageTitle.less'
-
-/** PageTitle
- *  @param props
- *  @param props.children
- *  @return {*}
+/**
+ * Main title of the page
+ *
+ * @param props
+ * @param props.children
+ * @return {*}
  */
 export function LuiPageTitle(props) {
 	return (
