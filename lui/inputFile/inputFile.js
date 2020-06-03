e0b5c59d1133d18928155e5f2ecf58e08155f604
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-03 23:08:36 +0300

    feat(LuiInputFile): set file url manually

--- lui/inputFile/inputFile.js
+++ lui/inputFile/inputFile.js
@@ -1,105 +1,111 @@
-/** File Uploader for Filoservo
- *  please note this component working only with https://github.com/ctdlspace/filoservo
+/**
+ * File Uploader for Filoservo
+ * @see https://github.com/ctdlspace/filoservo
  *
- *  @see https://github.com/ctdlspace/filoservo
- *  @author rusinozemtsev
- *  @version 0.0.1
+ * @author rusinozemtsev
+ * @version 0.0.1
+ *
  */
-
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { LuiThumbnail } from '../thumbnail/thumbnail'
+import { luiFireChange, luiFireEvent, luiUseState } from '../../helpers'
 import './inputFile.less'
+import { LuiInputText } from '../inputText'
 
-/** LuiInputFile
- *  @param props
- *  @param props.name
- *  @param props.value
- *  @param props.options
- *  @param props.options.server
- *  @param {function} props.onChange
- *  @return {*}
+/**
+ * Upload file to Filoservo and show preview
+ *
+ * @typedef {{}} LuiInputFileProps
+ * @property {string} name
+ * @property {string} value
+ * @property {string} options.server Filoservo url
+ * @property {boolean} [options.showUrlInput=false] allow to enter file url manually
+ * @property {function} onChange
+ *
+ * @param {LuiInputFileProps} props
+ * @return {*}
+ *
  */
 export const LuiInputFile = props => {
-  const { options: { server } } = props
-  const [value, setValue] = useState(props.value)
-  const [loading, setLoading] = useState(false)
-  const maxFiles = 20
-  const ref1 = React.createRef()
-  const onChange = e => {
-	const formData = new FormData()
-	for (let i in ref1.current.files) {
-	  if (i > maxFiles) {
-		throw Error(
-		  `form data contains to many files maxFiles set to ${maxFiles} vs ${ref1.current.files.length}`,
-		)
-	  }
-	  formData.append(
-		'upload',
-		ref1.current.files[i],
-	  )
+	const { value = '', options = {} } = props
+	const { server } = options
+	const [state, setState] = luiUseState({
+		value,
+		isLoading: false,
+	})
+	/** @type { {current: HTMLInputElement} } **/
+	const ref1 = React.createRef()
+	/** @param { FileservoResponse|string } dataOrString **/
+	const setData = dataOrString => {
+		const file = dataOrString?.files[0].nameWithOriginalName
+		const value = file ? `${server}/view/${file}` : dataOrString
+		setState({ value, isLoading: false })
 	}
-	const options = {
-	  method: 'POST',
-	  body: formData,
+	/** @param { LuiFormEvent } e */
+	const onChange = e => {
+		const formData = new FormData()
+		for (let i in ref1.current.files) {
+			formData.append('upload', ref1.current.files[i])
+		}
+		const options = {
+			method: 'POST',
+			body: formData,
+		}
+		setState({ isLoading: true })
+		fetch(`${server}/upload`, options)
+			.then(res => res.json())
+			.then(setData)
+			.catch(console.error)
 	}
-	setLoading(true)
-
-	fetch(`${server}/upload`, options)
-	  .then(res => res.json())
-	  .then(data => {
-		setLoading(false)
-		setValue(`${server}/view/${data.files[0].nameWithOriginalName}`)
-		props.onChange({
-		  target: {
-			name: props.name,
-			value: `${server}/view/${data.files[0].nameWithOriginalName}`,
-		  },
-		})
-	  })
-	  .catch(console.error)
-  }
-  if (value !== props.value) {
-	setValue(props.value)
-  }
-  return (
-	<Fragment>
-	  <input
-		type='file'
-		ref={ref1}
-		style={{ display: 'none' }}
-		onChange={onChange}/>
-	  <div className='luiInputFile'>
-		<div className='luiInputFile_preview'>
-		  <LuiThumbnail src={value || '/images/icons/upload.svg'}/>
-		</div>
-		{loading ? (
-		  <div className='luiInputFile_progress'>
-			Uploading <span className='luiInputFile_spinner'></span>
-		  </div>
-		) : (
-		  <div>
-			<button
-			  className='luiInputFile_link'
-			  onClick={e => {
-				e.preventDefault()
-				ref1.current.click()
-			  }}>
-			  Upload File
-			</button>
-			{value && (
-			  <div>
-				{value.split('?')[1] && (
-				  <a
-					href={`${value.replace('/view/', '/download/')}`}
-					target="_blank">
-					Download
-				  </a>
+	useEffect(() => {
+		props.value !== state.value && luiFireChange(props, state.value)
+	})
+	return (
+		<Fragment>
+			<input
+				ref={ref1}
+				style={{ display: 'none' }}
+				type='file'
+				onChange={onChange}/>
+			<div className='luiInputFile'>
+				<div className='luiInputFile_preview'>
+					<LuiThumbnail src={state.value || '/images/icons/upload.svg'}/>
+				</div>
+				{state.isLoading ? (
+					<div className='luiInputFile_progress'>
+						Uploading <span className='luiInputFile_spinner'/>
+					</div>
+				) : (
+					<div>
+						{options.showUrlInput && <div>
+							<LuiInputText
+								name={'url'}
+								value={state.value}
+								onChange={e => {
+									setState({ value: e.target.value })
+								}}/>
+						</div>}
+						<button
+							className='luiInputFile_link'
+							onClick={
+								e => {
+									e.preventDefault()
+									ref1.current.click()
+								}
+							}>Upload File
+						</button>
+						{state.value && (
+							<div>
+								{state.value.split('?')[1] && (
+									<a
+										href={`${state.value.replace('/view/', '/download/')}`}
+										target="_blank">Download</a>
+								)}
+							</div>
+						)}
+					</div>
 				)}
-			  </div>
-			)}
-		  </div>
-		)}
-	  </div>
-	</Fragment>
-  )
+			</div>
+		</Fragment>
+	)
 }
