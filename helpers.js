34b50ae6cd92548dcdf9517db8eceb1e93ef0bd5
Rus Inozemtsev <rus.inozemtsev@gmail.com>
2020-06-03 22:52:28 +0300

    feat(helpers): luiUseState, luiFireChange

--- helpers.js
+++ helpers.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 /** Creates className value for the component
  *  all booleans are interpreted as BEM modifications
  *
@@ -25,10 +26,11 @@ export const luiCls = (baseClassName = '', props = {}) => {
 	return { className: classes.join(' ') }
 }
 
-/** mapCom automatically does KEY stuff
- *  @param {[]} list
- *  @callback Com
- *  @returns {*}
+/**
+ * mapCom automatically does KEY stuff
+ * @param {[]} list
+ * @param {function} Com
+ * @returns {*}
  */
 export const luiMapCom = (list, Com) => {
 	return list.map((el, key) => {
@@ -40,6 +42,35 @@ export const luiMapCom = (list, Com) => {
 	})
 }
 
-export const luiCompareObj = (o1, o2) => {
+export const luiObjAreEq = (o1, o2) => {
 	return JSON.stringify(o1) === JSON.stringify(o2)
+}
+
+/**
+ * Send event using name taken from props
+ * @param props
+ * @param event
+ * @param value
+ */
+export const luiFireEvent = (props, event, value) => {
+	props[event]?.({
+		target: { name: props.name, value },
+	})
+}
+
+export const luiFireChange = (props, value) => {
+	luiFireEvent(props, 'onChange', value)
+}
+
+/**
+ * Patch state
+ * @param init
+ * @return {[{}, function({}): {}]}
+ */
+export const luiUseState = (init) => {
+	const [state, setState] = useState(init)
+	return [
+		state,
+		patch => setState({ ...state, ...patch }),
+	]
 }
